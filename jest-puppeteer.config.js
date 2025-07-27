9a74fe4e5ebaa0b6bf0734f448d185e638d3f22e
Esteban De La Fuente Rubio <esteban@delaf.cl>
2025-07-27 03:04:28 -0400

    Se quita ruta de chrome fija para usar la del test.

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,7 +2,6 @@ module.exports = {
     launch: {
         headless: 'new',
         slowMo: 100,
-        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
